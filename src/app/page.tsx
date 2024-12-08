7682285c75877e75cb92ca6db92f0ec01b53cab3
codewithzain099 <zainiikhan099@gmail.com>
2024-12-08 08:48:50 -0800

    added text in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ const HomePage = () => {
   return (
     <div className="container mx-auto min-h-[100vh] ">
       <HeroSection />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nesciunt obcaecati laboriosam suscipit? Eum illo, quia nostrum deserunt quisquam ipsum autem praesentium quaerat soluta, vero ut odio ratione suscipit perferendis.
+      </p>
     </div>
   );
 };
